d9f3bb4e687961f5dc7d2a4156c826686ee4ff9b
Nitinn-dev <gade.nitin1@outlook.com>
2025-04-19 09:49:28 +0530

    made changes

--- src/app/main/contact/page.jsx
+++ src/app/main/contact/page.jsx
@@ -1,23 +1,23 @@
-"use client"
-import { emailAdress, insta, whatsapp } from '../../../lib/requirement'
-
-const page = () => {
-  return (
-    // <div className='w-full h-[83vh] bg-[url("/mobile.jpeg")] bg-cover bg-center'>
-    <div className='w-full h-[82vh] bg-gradient-to-b from-blue-100 to-white'>
-      <div className="flex items-center justify-center h-full">
-
-        <div className='flex flex-col items-center w-[30vw] max-w-[500px] h-[27vh] max-h-[400px] '>
-        <div className='flex flex-row'>
-         <img src='/gmail.png'></img> <h1 className='ml-3 mt-1'>Email us at <a className='font-bold' href={`mailto:+${emailAdress}`} >{emailAdress}</a></h1></div>
-         <div className='flex flex-row justify-center items-center mb-1'>
-         <a href={`https://wa.me/+${whatsapp}`}><img src='/wa.png'/></a><a href={`https://ig.me/+${insta}`}><img src='/in.png'/></a> </div>
-         </div>
-
-    </div>
-    </div>
-
-  )
-}
-
-export default page
+ "use client"
+  import { emailAdress, insta, whatsapp } from '../../../lib/requirement'
+  
+  const page = () => {
+    return (
+      // <div className='w-full h-[83vh] bg-[url("/mobile.jpeg")] bg-cover bg-center'>
+      <div className='w-full h-[82vh] bg-gradient-to-b from-blue-100 to-white'>
+        <div className="flex items-center justify-center h-full">
+  
+          <div className='flex flex-col items-center w-[30vw] max-w-[500px] h-[27vh] max-h-[400px] '>
+          <div className='flex flex-row'>
+           <img src='/gmail.png'></img> <h1 className='ml-3 mt-1'>Email us at <a className='font-bold' href={`mailto:+${emailAdress}`} >{emailAdress}</a></h1></div>
+           <div className='flex flex-row justify-center items-center mb-1'>
+           <a href={`https://wa.me/+${whatsapp}`}><img src='/wa.png'/></a><a href={`https://ig.me/+${insta}`}><img src='/in.png'/></a> </div>
+           </div>
+  
+      </div>
+      </div>
+  
+    )
+  }
+  
+  export default page
